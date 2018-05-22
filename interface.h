25640c6ec6520b0fa146be45eea5b921d6ee127f
Guillaume Damiens <guillaume.damiens@etu.utc.fr>
2018-05-22 14:41:08 +0200

    Déplacement de l'interface de etat.cpp vers interface.cpp

---
+++ interface.h
@@ -0,0 +1,6 @@
+#if !defined(INTERFACE_H)
+#define INTERFACE_H
+
+int interface(int argc, char * argv[]);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "interface.h"
+int main(int argc, char * argv[]) {
+    interface(argc, argv);
+    return 0;
+}
