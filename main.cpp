912a2411e1b1b2ddac5bd36213725bbef82a4965
Solene Houlliez <solene.houlliez@etu.utc.fr>
2018-05-28 09:14:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-
 /*!
     * \file main.c
     * \brief Test des automates
     * \author Guillaume Damiens, Solène Houlliez, Oscar Roisin
     * \version 0.1
+    * \date 28 mai 2018
     *
-    * Programme pour tester les fonctions de l'automate et l'affichage
+    * \details Programme pour tester les fonctions de l'automate et l'affichage
     *
     */
     
@@ -13,10 +13,10 @@
 
 /*!
     * \fn int main (int argc,char * argv[])
-    *\brief Entrée du programme
+    * \brief Entrée du programme
+    * 
     *
-    *
-    *\return 0 - Arrête normal du programme
+    * \return 0 - Arrête normal du programme
     *
     */
     
