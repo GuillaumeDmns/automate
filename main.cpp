3fc823466e1c1e0d91888c2a205d20448048ea68
Solene Houlliez <solene.houlliez@etu.utc.fr>
2018-05-28 08:16:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+
+/*!
+    * \file main.c
+    * \brief Test des automates
+    * \author Guillaume Damiens, Solène Houlliez, Oscar Roisin
+    * \version 0.1
+    *
+    * Programme pour tester les fonctions de l'automate et l'affichage
+    *
+    */
+    
 #include "interface.h"
+
+/*!
+    * \fn int main (int argc,char * argv[])
+    *\brief Entrée du programme
+    *
+    *
+    *\return 0 - Arrête normal du programme
+    *
+    */
+    
 int main(int argc, char * argv[]) {
     interface(argc, argv);
     return 0;
