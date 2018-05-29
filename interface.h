383cfc1c75484120fbd045fc021673c44e5a143b
Solene Houlliez <solene.houlliez@etu.utc.fr>
2018-05-29 09:06:29 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,5 +1,18 @@
+/*!
+    * \file interface.h
+    * \brief Déclaration des classes et fonctions de l'interface
+    * \author Guillaume Damiens, Solène Houlliez, Oscar Roisin
+    * \version 0.1
+    * \date 28 mai 2018
+    *
+    * \details Programme pour déclarer les fonctions et les classes relatives à l'interface graphique
+    *
+    */
+
+
 #if !defined(INTERFACE_H)
 #define INTERFACE_H
+
 
 int interface(int argc, char* argv[]);
 
