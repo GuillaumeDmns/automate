ed5ab0f1a71b6f792669021924f325630d42f81d
Solene Houlliez <solene.houlliez@etu.utc.fr>
2018-05-29 09:02:13 +0200

    Update etat.cpp

--- etat.cpp
+++ etat.cpp
@@ -1,9 +1,30 @@
+/*!
+    * \file etat.cpp
+    * \brief Définition des fonctions de l'objet Etat
+    * \author Guillaume Damiens, Solène Houlliez, Oscar Roisin
+    * \version 0.1
+    * \date 28 mai 2018
+    *
+    * \details Fichier pour définir les fonctions relatives à Etat non inline
+    *
+    */
+
+
 #include <stdio.h>
 #include <iostream>
 #include "etat.h"
 
 using namespace std;
 
+/*!
+    * \fn Etat1D::Etat1D (unsigned int n,unsigned int* t=0)
+    * \brief Constructeur de la classe Etat1D
+    * 
+    * \param unsigned int n : dimension de la grille
+    * \param unsigned int* t : tableau des valeurs (par défaut =0)
+    * \return 
+    *
+    */
 Etat1D::Etat1D (unsigned int n,unsigned int* t=0): Etat(n), valeur(new unsigned int[n]){
     if(t)
     {
@@ -16,6 +37,16 @@ Etat1D::Etat1D (unsigned int n,unsigned int* t=0): Etat(n), valeur(new unsigned 
     }
 }
 
+/*!
+    * \fn Etat2D::Etat2D (unsigned int n,unsigned int m,unsigned int** t=0)
+    * \brief Constructeur de la classe Etat2D
+    * 
+    * \param unsigned int n : dimension N de la grille
+    * \param unsigned int m : dimension M de la grille
+    * \param unsigned int** t : tableau des valeurs (par défaut =0)
+    * \return 
+    *
+    */
 Etat2D::Etat2D(unsigned int n,unsigned int m,unsigned int** t=0):Etat(n),dimM(m),valeur(new unsigned int*[n]){
     if (t){
         for (unsigned int i=0;i<(n);i++)
@@ -35,6 +66,14 @@ Etat2D::Etat2D(unsigned int n,unsigned int m,unsigned int** t=0):Etat(n),dimM(m)
     }
 }
 
+/*!
+    * \fn Etat2D::~Etat2D()
+    * \brief Destructeur de la classe Etat2D
+    * 
+    * 
+    * \return 
+    *
+    */
 Etat2D::~Etat2D(){
     for (unsigned int i=0;i<this->getdimN();i++)
         {
